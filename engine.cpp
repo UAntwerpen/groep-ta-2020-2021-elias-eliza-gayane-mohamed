aeea3a809cbcf1a435cea69bd8cb5b7f4dc022b2
Elias-Dams <72469787+Elias-Dams@users.noreply.github.com>
2021-05-11 16:09:18 +0200

    Add files via upload

---
+++ engine.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include "automata/DFA_elias.h"
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+    for(int i = 1; i < argc; i++){
+        ifstream txt_file;
+        txt_file.open(argv[i]);
+        string line;
+        if (txt_file.is_open()) {
+            while (getline(txt_file, line)) {
+                cout << line << endl;
+            }
+        }
+        txt_file.close();
+    }
+    return 0;
+}
